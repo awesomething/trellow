4305e733ba9252cfcc809b9896f888c3c7fef69b
Awesome Henry <henryebomah@gmail.com>
2020-12-28 09:44:52 -0800

    Initial

---
+++ src/Card.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import './Card.css';
+
+export default function Card(props){
+    return (
+        <div className='Card'>
+            <button
+            type='button'
+            >
+                Delete
+            </button>
+    {/* add curly braces to make it a string */}
+          <h3>{props.title}</h3>
+          <p>{props.content}</p>
+        </div>
+    )}
