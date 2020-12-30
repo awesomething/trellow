df601e41613859b850fb3f0ef01869c5c2d500ca
Henry Ebomah <henryebomah@gmail.com>
2020-12-30 13:18:34 -0800

    Test

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
-import './index.css';
+import STORE from './STORE.js';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(<App store={STORE}/>, document.getElementById('root'));
